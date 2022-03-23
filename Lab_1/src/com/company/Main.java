56bd8ef33015ef281f15aedcf5b63eaa2cb06372
Ангелина <luchkina.angela2001@mail.ru>
2022-03-23 15:48:18 +0300

    Add project lab_1

---
+++ Lab_1/src/com/company/Main.java
@@ -0,0 +1,22 @@
+/*
+import java.util.Scanner;
+
+public class Program{
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите свое имя: ");
+        String name = in.nextLine();
+        System.out.printf("Hello, %s");
+        in.close();
+    }
+}
+*/
+public class Program {
+
+    public static void main (String args[]){
+
+        System.out.println("Hello Java!");
+    }
+}
